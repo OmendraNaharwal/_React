3ed2a51f19981274be60a1fb69eab7e9c98937c5
OmendraNaharwal <omendraji03@gmail.com>
2025-08-24 19:18:32 +0530

    feat: add onDeleteClick handler to Todoitem component for delete functionality

--- projects/src/components/Todoitem.jsx
+++ projects/src/components/Todoitem.jsx
@@ -1,11 +1,11 @@
-function Todoitem({todoname,duedate}){
+function Todoitem({todoname,duedate,onDeleteClick}){
   return (
-    <div>
+    <div style={{borderRadius: "2px", padding: "8px", marginBottom: "8px"}}>
         <div className="row">
-          <div className="col">
+          <div className="col" style={{marginTop: "4px"}}>
             {todoname}
           </div>
-          <div className="col-5">
+          <div className="col-5" style={{marginTop: "4px"}}>
             {duedate}
           </div>
           <div className="col">
@@ -13,12 +13,15 @@ function Todoitem({todoname,duedate}){
               type="button"
               className="btn btn-primary"
               style={{
-                "--bs-btn-padding-y": ".25rem",
-                "--bs-btn-padding-x": ".5rem",
-                "--bs-btn-font-size": ".75rem"
-                , backgroundColor: "red",
+                // "--bs-btn-padding-y": ".25rem",
+                // "--bs-btn-padding-x": ".5rem",
+                // "--bs-btn-font-size": ".75rem"
+                width: "100%",
+                marginTop: "4px",
+                backgroundColor: "red",
                 borderColor: "red",
               }}
+              onClick={() => onDeleteClick(todoname)}
             >
               Delete
             </button>
