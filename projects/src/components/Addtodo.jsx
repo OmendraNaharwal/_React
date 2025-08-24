b08968772cddd8a2e498771a81f3898412ac02cb
OmendraNaharwal <omendraji03@gmail.com>
2025-08-24 19:18:18 +0530

    feat: enhance Addtodo component with state management for todo name and due date

--- projects/src/components/Addtodo.jsx
+++ projects/src/components/Addtodo.jsx
@@ -1,30 +1,63 @@
-function Addtodo(){
+import { useState } from "react";
+
+function Addtodo({ onNewitem }) {
+  const [todoname, settodoname] = useState('');
+  const [duedate, setduedate] = useState('');
+
+  const handlenamechange = (event) => {
+    settodoname(event.target.value);
+  };
+
+  const handleduedatechange = (event) => {
+    setduedate(event.target.value);
+  };
+
+  const handleaddbuttonclicked = () => {
+    if (!todoname || !duedate) return;
+    onNewitem(todoname, duedate);
+    settodoname('');
+    setduedate('');
+  };
+
   return (
     <div>
-        <div className="row">
-          <div className="col">
-            <input type="text" placeholder="Enter Todo Here" />
-          </div>
-          <div className="col-5">
-            <input type="date" />
-          </div>
-          <div className="col">
-            <button
-              type="button"
-              className="btn btn-primary"
-              style={{
-                "--bs-btn-padding-y": ".25rem",
-                "--bs-btn-padding-x": ".5rem",
-                "--bs-btn-font-size": ".75rem"
-                , backgroundColor: "green",borderColor: "green",
-              }}
-            >
-              Add
-            </button>
-          </div>
+      <div className="row">
+        <div className="col">
+          <input
+            type="text"
+            placeholder="Enter Todo Here"
+            value={todoname}
+            onChange={handlenamechange}
+          />
+        </div>
+        <div className="col-5">
+          <input
+            type="date"
+            value={duedate}
+            onChange={handleduedatechange}
+          />
+        </div>
+        <div className="col">
+          <button
+            type="button"
+            className="btn btn-primary"
+            style={{
+              // "--bs-btn-padding-y": ".25rem",
+              // "--bs-btn-padding-x": ".5rem",
+              // "--bs-btn-font-size": ".75rem",
+              width: "100%",
+              marginTop: "4px",
+              backgroundColor: "green",
+              borderColor: "green",
+            }}
+            onClick={handleaddbuttonclicked}
+          >
+            Add
+          </button>
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
 export default Addtodo;
