005c70ccfa465f6326c014a2dfa22955d86bc931
OmendraNaharwal <omendraji03@gmail.com>
2025-08-27 16:48:34 +0530

    gvc

--- Project7/src/components/Postlist.jsx
+++ Project7/src/components/Postlist.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
 import Post from "./Post";
+import { PostListContext } from "../store/post-list-store";
 
 const Postlist = () => {
+  const { postlist } = useContext(PostListContext);
+
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
+      {postlist.map((post) => (<Post key={post.id} post={post} />
+    ))}
     </div>
   );
 }
