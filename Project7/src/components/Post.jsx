055ac1171adc86f4eccc6486fd3bcd0cf170e717
OmendraNaharwal <omendraji03@gmail.com>
2025-08-27 16:48:29 +0530

    9876

--- Project7/src/components/Post.jsx
+++ Project7/src/components/Post.jsx
@@ -1,10 +1,11 @@
-const Post=()=>{
+const Post=({post})=>{
   return (
-    <div className="card" style={{ margin: "10px", padding: "20px" }}>
+    <div className="card post-card" 
+    style={{ margin: "10px", padding: "20px", width: "30rem" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">{post.title}</h5>
+        <p className="card-text">{post.body}</p>
+        
       </div>
     </div>
   )
