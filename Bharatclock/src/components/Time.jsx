5ae61701d4470fd1b22132e4ae072f340937dbf8
OmendraNaharwal <omendraji03@gmail.com>
2025-08-29 00:18:03 +0530

    rfdfgh

--- Bharatclock/src/components/Time.jsx
+++ Bharatclock/src/components/Time.jsx
@@ -1,6 +1,18 @@
+import { use, useEffect, useState } from "react";
+
 function Time(){
+
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (  
-    <p>This is the current date and time: {new Date().toLocaleDateString("en-IN", { timeZone: "Asia/Kolkata" })} - {new Date().toLocaleTimeString("en-IN", { timeZone: "Asia/Kolkata" })}</p>
+    <p>This is the current date and time: {time.toLocaleDateString("en-IN", { timeZone: "Asia/Kolkata" })} - {time.toLocaleTimeString("en-IN", { timeZone: "Asia/Kolkata" })}</p>
   )
 }
 export default Time;
